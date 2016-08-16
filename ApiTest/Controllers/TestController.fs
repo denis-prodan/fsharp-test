66a2debf60abe236495a2bffe8fa212fa0ed05c8
Denys Prodan <denys.prodan@avanade.com>
2016-08-16 11:10:32 +0300

    First simple implementation

---
+++ ApiTest/Controllers/TestController.fs
@@ -0,0 +1,26 @@
+﻿namespace ApiTest.Controllers
+
+open ApiTest.Models
+open System
+open System.Web.Http
+open System.Net.Http
+
+type TestController()  = 
+    inherit ApiController()
+
+    [<RouteAttribute("test/get")>]
+    [<HttpGetAttribute>]
+    member this.Test() = { TestData.ControllerName = "testcontroller"; Id = 4 }
+    
+    [<RouteAttribute("test/getHttp")>]
+    [<HttpGetAttribute>]
+    member this.TestHttpClient() = Async.RunSynchronously (async {
+        let httpClient = new HttpClient (BaseAddress = new Uri("http://localhost:48213/"))
+        let! response = httpClient.GetAsync("test/get") |> Async.AwaitTask
+        
+        response.EnsureSuccessStatusCode |> ignore
+
+        let! result = response.Content.ReadAsAsync<TestData>() |> Async.AwaitTask
+        let alteredResult = { result with ControllerName = "alteredController" }
+        return alteredResult
+    })
---
+++ ApiTest/Models/TestData.fs
@@ -0,0 +1,11 @@
+﻿namespace ApiTest.Models
+
+open Newtonsoft.Json
+
+[<JsonObject(MemberSerialization=MemberSerialization.OptOut)>]
+type TestData = {
+    [<JsonPropertyAttribute("controllerName")>]
+    ControllerName : string;
+
+    [<JsonPropertyAttribute("id")>]
+    Id : int }
